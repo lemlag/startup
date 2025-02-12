154e0787edc99449006b813b15d3b5fe59911397
John Rowberry <johnrowberries@gmail.com>
2025-02-12 10:52:46 -0700

    Ported "login"

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import './login.css'
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main>
+
+    <section class="signup">
+
+      <img id="lock" src="./Lock.jpg" alt="Lock icon"/>
+
+      <form method="get" action="/">
+          <div>
+            <span>Username:</span>
+            <input type="text" placeholder="username" />
+          </div>
+          <div>
+            <span>Password:</span>
+            <input type="password" placeholder="password" />
+          </div>
+          <div>
+          <button type="submit">Login</button>
+          <button type="submit">Create</button>
+          </div>
+      </form>
+
+    </section>
     </main>
   );
 }
