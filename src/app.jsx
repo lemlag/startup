852790f97ece77e09e5330f461db2f98f2ff22e4
John Rowberry <johnrowberries@gmail.com>
2025-02-12 10:22:04 -0700

    Created router

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
 
 export default function App() {
-  return <div className="body bg-dark text-light">
+  return (
+  <BrowserRouter>
+  <div className="body bg-dark text-light">
     <header>
       <h1>Sudoku Central</h1>
     </header>
 
-    <main> 
+  <Routes>
+    <Route path='/login' element={<Login />} exact />
+    <Route path='/' element={<Play />} />
+    <Route path='/scores' element={<Scores />} />
+    <Route path='*' element={<NotFound />} />
+  </Routes>
+
+    {/* <main> 
       <div>App components go here </div>
 
           {/* -- Navigation elements -- */}
     <aside>
         <nav>
           <menu>
-            <li><a id="home" href="index.html">Home</a></li>
-            <li><a id="score" href="scores.html">Scores</a></li>
-            <li><a id="login" href="login.html">Log In/Sign Up</a></li>
-            <li><a>User: Username</a></li>
+            <li><NavLink id="home" to="">Home</NavLink></li>
+            <li><NavLink id="score" to="scores">Scores</NavLink></li>
+            <li><NavLink id="login" to="login">Log In/Sign Up</NavLink></li>
+            <li>User: Username</li>
           </menu>
         </nav>
     </aside>
-    </main>
+    {/* </main> */}
 
 
 
@@ -30,8 +43,14 @@ export default function App() {
       <hr />
       <span>John Rowberry</span>
       <br />
-      <a href="https://github.com/lemlag/startup">GitHub</a>
+      <NavLink to="https://github.com/lemlag/startup">GitHub</NavLink>
     </footer>
 
   </div>;
+  </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
