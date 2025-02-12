c06e8557486b5671ef1678cd30b1c130422626df
John Rowberry <johnrowberries@gmail.com>
2025-02-12 00:12:12 -0700

    Create app component

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return <div className="body bg-dark text-light">
+    <header>
+      <h1>Sudoku Central</h1>
+    </header>
+
+    <main> 
+      <div>App components go here </div>
+
+          {/* -- Navigation elements -- */}
+    <aside>
+        <nav>
+          <menu>
+            <li><a id="home" href="index.html">Home</a></li>
+            <li><a id="score" href="scores.html">Scores</a></li>
+            <li><a id="login" href="login.html">Log In/Sign Up</a></li>
+            <li><a>User: Username</a></li>
+          </menu>
+        </nav>
+    </aside>
+    </main>
+
+
+
+    <footer>
+      <hr />
+      <span>John Rowberry</span>
+      <br />
+      <a href="https://github.com/lemlag/startup">GitHub</a>
+    </footer>
+
+  </div>;
 }
