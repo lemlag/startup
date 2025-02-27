1124ed2785c56a2e88a6f090fcc9ce4b010ac28c
John Rowberry <johnrowberries@gmail.com>
2025-02-27 01:25:05 -0700

    Unauthenticated Login

---
+++ src/login/unauthenticated.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export function Unauthenticated(props) {
+  const [userName, setUserName] = React.useState(props.userName);
+  const [password, setPassword] = React.useState('');
+  const [displayError, setDisplayError] = React.useState(null);
+
+  async function loginUser() {
+    localStorage.setItem('userName', userName);
+    props.onLogin(userName);
+  }
+
+  async function createUser() {
+    localStorage.setItem('userName', userName);
+    localStorage.setItem('password', password);
+    props.onLogin(userName);
+  }
+
+  return (
+    <section class="signup">
+      <img id="lock" src="./Lock.jpg" alt="Lock icon"/>
+      <div>
+        <span>
+          Username:
+        </span>
+        <input type="text" value="userName" onChange={(e) => setUserName(e.target.value)} placeholder="username@email.com" />
+      </div>
+      <div>
+        <span>
+          Password:
+        </span>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="password" />
+      </div>
+      <div>
+        <button onClick={() => loginUser()} disabled={!userName || !password}>
+          Login
+        </button>
+        <button onClick={() => createUser()} disabled={!userName || !password}>
+          Create
+        </button>
+      </div>
+      
+      <MessageDialog message={displayError} onHide={() => setDisplayError(null)} />
+    </section>
+  );
+}
+
